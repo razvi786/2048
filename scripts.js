194b3663bdca5e4c15adb65200f8534a9ac11223
Syed Ehsan Ali Razvi <ehsan.razvi92@gmail.com>
2020-03-03 00:07:57 +0530

    Version 2

--- scripts.js
+++ scripts.js
@@ -1,95 +1,130 @@
-function hello(){
+function hello() {
   alert("hello")
 }
 
-$(document).keydown(function(){
+$(document).keydown(function () {
   move(event);
 });
 
-function start(){
-  
-  document.getElementById("4").classList.remove("bg-secondary")
-  document.getElementById("4").classList.add("bg-primary")
-  document.getElementById("4").classList.add("selected")
-  
+function select(ele){
+  ele.classList.remove("bg-secondary")
+  ele.classList.add("bg-primary")
 }
 
-function move(e){
-  var key=e.which? e.which:e.keyCode;
+function remove(ele){
+  ele.classList.remove("bg-primary")
+  ele.classList.add("bg-secondary")
+}
+
+function move(e) {
+  var key = e.which ? e.which : e.keyCode;
   // alert(key)
-  if(key==37){
+  if (key == 37) {
     left();
   }
-  if(key==39){
+  if (key == 39) {
     right();
   }
-  if(key==38){
+  if (key == 38) {
     up();
   }
-  if(key==40){
+  if (key == 40) {
     down();
   }
 }
 
-function left(){
-  var sel=document.getElementsByClassName("bg-primary")[0];
-  var next=sel.previousElementSibling;
+function left() {
+  var sels = document.getElementsByClassName("bg-primary");
+  for (let i = 0; i < sels.length; i++) {
+    var sel = sels[i];
+    var next = sel.previousElementSibling;
 
-  if(document.getElementById("first")!=next){
-    sel.classList.remove("bg-primary")
-  sel.classList.add("bg-secondary")
-  
-  
-  next.classList.remove("bg-secondary");
-  next.classList.add("bg-primary");
+    if (document.getElementById("first") != sel) {
+      if (document.getElementById("3") != next) {
+        if (document.getElementById("6") != next) {
+          if (sels[i - 1] != next) {
+            sel.classList.remove("bg-primary")
+            sel.classList.add("bg-secondary")
+
+            next.classList.remove("bg-secondary");
+            next.classList.add("bg-primary");
+          }
+        }
+      }
+    }
+
   }
 
-  
 
-  
+
+
 }
 
-function right(){
-  var sel=document.getElementsByClassName("bg-primary")[0];
-  var next=sel.nextElementSibling;
+function right() {
+  var sels = document.getElementsByClassName("bg-primary");
+  for (let i = sels.length - 1; i >= 0; i--) {
 
-  if(document.getElementById("last")!=next){
-  sel.classList.remove("bg-primary")
-  sel.classList.add("bg-secondary")
-  
-  
-  next.classList.remove("bg-secondary");
-  next.classList.add("bg-primary");
-  
+    var sel = sels[i];
+
+    var next = sel.nextElementSibling;
+    // alert("selected:"+sel.id+" next selected:"+sels[i+1].id+" next:"+next.id)
+    if (document.getElementById("4") != next) {
+      if (document.getElementById("7") != next) {
+        if (document.getElementById("last") != next) {
+
+          if (sels[i + 1] != next) {
+
+            sel.classList.remove("bg-primary")
+            sel.classList.add("bg-secondary")
+
+            next.classList.remove("bg-secondary");
+            next.classList.add("bg-primary");
+          }
+        }
+      }
+    }
   }
 }
-function up(){
-  var sel=document.getElementsByClassName("bg-primary")[0];
-  var next=sel.previousElementSibling.previousElementSibling.previousElementSibling;
+function up() {
+  var sels = document.getElementsByClassName("bg-primary");
+  for (let i = 0; i < sels.length; i++) {
+    var sel = sels[i];
+    var next = sel.previousElementSibling.previousElementSibling.previousElementSibling;
 
-  if(document.getElementById("first")!=next){
-  sel.classList.remove("bg-primary")
-  sel.classList.add("bg-secondary")
-  
-  
-  next.classList.remove("bg-secondary");
-  next.classList.add("bg-primary");
+    if (document.getElementById("first") != sel) {
+      if (sels[i - 1] != next && sels[i - 2] != next && sels[i - 3] != next) {
+        sel.classList.remove("bg-primary")
+        sel.classList.add("bg-secondary")
+
+        next.classList.remove("bg-secondary");
+        next.classList.add("bg-primary");
+      }
+    }
   }
-  
+
 }
-function down(){
-  var sel=document.getElementsByClassName("bg-primary")[0];
-  var next=sel.nextElementSibling.nextElementSibling.nextElementSibling;
+function down() {
+  var sels = document.getElementsByClassName("bg-primary");
+  for (let i = sels.length - 1; i >= 0; i--) {
 
-  if(document.getElementById("last")!=next){
-  sel.classList.remove("bg-primary")
-  sel.classList.add("bg-secondary")
-  
-  
-  next.classList.remove("bg-secondary");
-  next.classList.add("bg-primary");
+    var sel = sels[i];
+
+    var next = sel.nextElementSibling.nextElementSibling.nextElementSibling;
+    // alert("selected:"+sel.id+" next selected:"+sels[i+1].id+" next:"+next.id)
+    if (document.getElementById("last") != sel) {
+
+      if (sels[i - 1] != next && sels[i - 2] != next && sels[i - 3] != next) {
+
+        // alert("work")
+        sel.classList.remove("bg-primary")
+        sel.classList.add("bg-secondary")
+
+        next.classList.remove("bg-secondary");
+        next.classList.add("bg-primary");
+      }
+    }
   }
-  
+
 }
 
 
